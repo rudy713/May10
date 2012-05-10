d4fe413cb8ee0c904b1fe09ddf23108ab6fe19e4
Rudy Pospisil <rudypospisil@gmail.com>
2012-05-10 01:02:51 -0400

    first commit

---
+++ www/functions.js
@@ -0,0 +1,20 @@
+function echoMenu() {
+  document.write('<div id="menu">');
+  document.write('<ul>');
+  document.write('<li><a href="index.html"><img src="flag_brazil.png" /></li>');
+  document.write('<li><a href="china.html"><img src="flag_china.png" /></li>');
+  document.write('<li><a href="india.html"><img src="flag_india.png" /></li>');
+  document.write('<li><a href="mexico.html"><img src="flag_mexico.png" /></li>');
+  document.write('<li><a href="southAfrica.html"><img src="flag_southAfrica.png" /></li>');
+  document.write('</ul>');
+  document.write('</div>');
+}      
+
+function echoFunFacts() {
+  document.write('<p>Fun facts available in the <em><a target="_blank" href="">CIA World Book</a></em>');
+  document.write('<br />');
+  document.write('<ol>');
+  document.write('<li>Population: </li>');
+  document.write('<li>GNP: </li>');
+  document.write('</ol>');
+}
